888be027f1ac69426fdbf70995bf3d1104412c6f
Ferdinand Beyer <fb@fbeyer.com>
2020-05-02 12:07:58 +0200

    Add ingredient list

--- src/main/bakermath/ui.cljs
+++ src/main/bakermath/ui.cljs
@@ -13,17 +13,52 @@
 
 (def app-bar (comp/factory AppBar))
 
-(defsc Button [this props]
-  (material/button {:variant "contained"
-                    :color "primary"
-                    :onClick (:act props)}
-                   (:text props)))
+(defsc Ingredient [this {:ingredient/keys [name quantity]}]
+  {:initial-state (fn [{:keys [name quantity]}]
+                    #:ingredient{:name name
+                                 :quantity quantity})}
+  (material/list-item
+   {:button true}
+   (material/list-item-text {:primary name,
+                             :secondary quantity})))
 
-(def button (comp/factory Button))
+(def ingredient (comp/factory Ingredient {:keyfn :ingredient/name}))
+
+(defsc IngredientList [this {:list/keys [name ingredients]}]
+  {:initial-state
+   (fn [{:keys [name]}]
+     {:list/name name
+      :list/ingredients
+      (letfn [(ingredient [[name quantity]]
+                (comp/get-initial-state Ingredient {:name name
+                                                    :quantity quantity}))]
+        (case name
+          "Sourdough"
+          (map ingredient
+               [["Rye Full Grain" "100 g"]
+                ["Rye 1150" "50 g"]
+                ["Water" "150 g"]
+                ["Starter" "15 g"]])
+          "Main dough"
+          (map ingredient
+               [["Rye 1150" "200 g"]
+                ["Wheat 1050" "100 g"]
+                ["Wheat Full Grain" "50 g"]
+                ["Water" "175 g"]
+                ["Salt" "9 g"]])))})}
+  (material/list
+   {}
+   (concat [(material/list-subheader {} name)]
+    (map ingredient ingredients))))
+
+(def ingredient-list (comp/factory IngredientList))
 
 (defsc Root [this props]
+  {:initial-state
+   (fn [params] {:sourdough (comp/get-initial-state IngredientList {:name "Sourdough"})
+                 :main (comp/get-initial-state IngredientList {:name "Main dough"})})}
   (dom/div
    (app-bar)
-   (dom/div {:style {:margin-top "84px"}}
-    (button {:text "I like my button..."
-             :act #(println "Clicked")}))))
+   (dom/div {:style {:margin-top "64px"}}
+            (ingredient-list (:sourdough props))
+            (ingredient-list (:main props)))))
--- src/main/bakermath/ui/material.cljs
+++ src/main/bakermath/ui/material.cljs
@@ -1,14 +1,23 @@
 (ns bakermath.ui.material
+  (:refer-clojure :exclude [list])
   (:require
    [com.fulcrologic.fulcro.algorithms.react-interop :as interop]
    ["@material-ui/core/AppBar" :as app-bar]
    ["@material-ui/core/Button" :as button]
+   ["@material-ui/core/List" :as list]
+   ["@material-ui/core/ListItem" :as list-item]
+   ["@material-ui/core/ListItemText" :as list-item-text]
+   ["@material-ui/core/ListSubheader" :as list-subheader]
    ["@material-ui/core/Paper" :as paper]
    ["@material-ui/core/ToolBar" :as tool-bar]
    ["@material-ui/core/Typography" :as typography]))
 
 (def app-bar (interop/react-factory app-bar/default))
 (def button (interop/react-factory button/default))
+(def list (interop/react-factory list/default))
+(def list-item (interop/react-factory list-item/default))
+(def list-item-text (interop/react-factory list-item-text/default))
+(def list-subheader (interop/react-factory list-subheader/default))
 (def paper (interop/react-factory paper/default))
 (def tool-bar (interop/react-factory tool-bar/default))
 (def typography (interop/react-factory typography/default))
