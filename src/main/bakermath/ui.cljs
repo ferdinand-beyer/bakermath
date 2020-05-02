7512054b8719632fbbcfbe73ab55e297b0c7034f
Ferdinand Beyer <fb@fbeyer.com>
2020-05-02 21:27:40 +0200

    Make ingredients separate entities

--- src/main/bakermath/ui.cljs
+++ src/main/bakermath/ui.cljs
@@ -6,6 +6,14 @@
    [com.fulcrologic.fulcro.dom :as dom]
    [com.fulcrologic.fulcro.mutations :as m :refer [defmutation]]))
 
+(def ingredients {:rfg "Rye Full Grain"
+                  :r1150 "Rye 1150"
+                  :wfg "Wheat Full Grain"
+                  :w1050 "Wheat 1050"
+                  :starter "Sourdough Starter"
+                  :w "Water"
+                  :s "Salt"})
+
 (defsc AppBar [this props]
   (material/app-bar
    {}
@@ -15,18 +23,27 @@
 
 (def app-bar (comp/factory AppBar))
 
-(defsc Ingredient
+(defsc Ingredient [this props]
+  {:query [:ingredient/id :ingredient/name]
+   :ident [:ingredient/id :ingredient/id]
+   :initial-state
+   (fn [id] #:ingredient{:id id
+                         :name (get ingredients id)})})
+
+(defsc Item
   [this
-   {:item/keys [id quantity]
+   {:item/keys [id quantity ingredient]
     {name :ingredient/name} :item/ingredient
     :as props}
    {:keys [on-delete]}]
-  {:query [:item/id :item/quantity {:item/ingredient [:ingredient/name]}]
+  {:query [:item/id :item/quantity
+           {:item/ingredient (comp/get-query Ingredient)}]
    :ident (fn [] [:item/id (:item/id props)])
-   :initial-state (fn [{:keys [id name quantity]}]
+   :initial-state (fn [{:keys [id ingredient quantity]}]
                     {:item/id id
                      :item/quantity quantity
-                     :item/ingredient {:ingredient/name name}})}
+                     :item/ingredient
+                     (comp/get-initial-state Ingredient ingredient)})}
   (material/list-item
    {:button true}
    (material/list-item-avatar
@@ -41,35 +58,35 @@
       :onClick #(on-delete id)}
      (material/delete-icon {})))))
 
-(def ingredient (comp/factory Ingredient {:keyfn :item/id}))
+(def ingredient (comp/factory Item {:keyfn :item/id}))
 
 (defsc IngredientList
   [this {:list/keys [id name items]
          :ui/keys [editing]
          :as props}]
   {:query [:list/id :list/name :ui/editing
-           {:list/items (comp/get-query Ingredient)}]
+           {:list/items (comp/get-query Item)}]
    :ident (fn [] [:list/id (:list/id props)])
    :initial-state
    (fn [{:keys [id name]}]
      {:list/id id
       :list/name name
       :list/items
-      (let [keys [:id :name :quantity]
-            item-state #(comp/get-initial-state Ingredient (zipmap keys %))]
+      (let [keys [:id :ingredient :quantity]
+            item-state #(comp/get-initial-state Item (zipmap keys %))]
         (case id
           1 (mapv item-state
-                  [[1 "Rye Full Grain" "100 g"]
-                   [2 "Rye 1150" "50 g"]
-                   [3 "Water" "150 g"]
-                   [4 "Starter" "15 g"]])
+                  [[1 :rfg "100 g"]
+                   [2 :r1150 "50 g"]
+                   [3 :w "150 g"]
+                   [4 :starter "15 g"]])
 
           2 (mapv item-state
-                  [[5 "Rye 1150" "200 g"]
-                   [6 "Wheat 1050" "100 g"]
-                   [7 "Wheat Full Grain" "50 g"]
-                   [8 "Water" "175 g"]
-                   [9 "Salt" "9 g"]])))})}
+                  [[5 :r1150 "200 g"]
+                   [6 :w1050 "100 g"]
+                   [7 :wfg "50 g"]
+                   [8 :w "175 g"]
+                   [9 :s "9 g"]])))})}
   (let [delete (fn [item-id]
                  (comp/transact! this
                                  [(mut/delete-item {:list/id id
