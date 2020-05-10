080c32d3082e49ee2ec064b79459d5b2bbe3b359
Ferdinand Beyer <fb@fbeyer.com>
2020-05-10 09:52:11 +0200

    Require vectors in spec

--- src/main/bakermath/db.cljs
+++ src/main/bakermath/db.cljs
@@ -10,20 +10,24 @@
 (s/def :editor/dough-ref nat-int?)
 
 (s/def :dough/name string?)
-(s/def :dough/ingredients (s/* ::dough-ingredient))
+(s/def :dough/ingredients
+  (s/coll-of ::dough-ingredient :kind vector?))
 
 (s/def :recipe/name string?)
-(s/def :recipe/doughs (s/* ::dough))
+(s/def :recipe/doughs
+  (s/coll-of ::dough :kind vector?))
 
 (s/def ::dough-ingredient
   (s/keys :req [:ingredient/name
                 :dough-ingredient/quantity]))
 
-(s/def ::dough (s/keys :req [:dough/name]
-                       :opt [:dough/ingredients]))
+(s/def ::dough
+  (s/keys :req [:dough/name]
+          :opt [:dough/ingredients]))
 
-(s/def ::recipe (s/keys :req [:recipe/name]
-                        :opt [:recipe/doughs]))
+(s/def ::recipe
+  (s/keys :req [:recipe/name]
+          :opt [:recipe/doughs]))
 
 (s/def ::dough-ingredient-editor
   (s/keys :req [:editor/visible
@@ -32,9 +36,11 @@
           :opt [:ingredient/name
                 :dough-ingredient/quantity]))
 
-(s/def ::root (s/keys :opt-un [::dough-ingredient-editor]))
+(s/def ::root
+  (s/keys :opt-un [::dough-ingredient-editor]))
 
-(s/def ::db (s/merge ::root ::recipe))
+(s/def ::db
+  (s/merge ::root ::recipe))
 
 (def default-db
   {:recipe/name "Bread"
